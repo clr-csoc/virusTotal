2a23f805bd26cf3e0725189bc7d75ce21278cd7a
clr-csoc <ernesto.ruiz@claro.com.pe>
2025-04-22 15:40:31 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup, find_packages
 
 
 if __name__ == "__main__":
-    setup(name ="ucprotect3",
-          description="Esta es una aplicacion para descargar level 3 de ucprotect",
+    setup(name ="virusTotal",
+          description="Esta es una aplicacion hecha en python para usar virustotal",
           license="MIT",
-          url="https://github.com/raulengineer/paquete.git",
+          url="https://github.com/clr-csoc/virusTotal.git",
           version="0.0.1",
           author="Raul Villano Obregon",
           author_email="raulengineer@gmail.com",
           long_description=open('README.md').read(),
           packages=find_packages(),
           zip_safe=False,
-          install_requires=["pandas","Beautifulsoup4","selenium","lxml"],
+          install_requires=["pandas","vt-py","openpyxl","numpy"],
         classifiers=[    "Development Status :: 5 - Production/Stable",
     "Intended Audience :: Developers",
     "Natural Language :: English",
