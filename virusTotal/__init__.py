033e985eee5338206b95a81d89d07bba67b2663f
clr-csoc <ernesto.ruiz@claro.com.pe>
2025-04-22 15:32:24 -0500

    update init

--- virusTotal/__init__.py
+++ virusTotal/__init__.py
@@ -2,5 +2,5 @@ __title__='paquete'
 __version__='0.0.1'
 __author__='Raul Villano'
 __email__='raulengineer@gmail.com'
-__description__='Este es un modulo de prueba para ejecutar ucprotect'
-__url__="https://github.com/raulengineer/paquete.git"
+__description__='Este es un modulo de prueba para ejecutar virus total con python'
+__url__="https://github.com/clr-csoc/virusTotal.git"
